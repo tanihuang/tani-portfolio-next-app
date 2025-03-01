89517181753260c970de5bf3351695f61f99c62f
Tani <tannihuang@gmail.com>
2025-03-02 01:51:08 +0800

    remove asset prefix

--- next.config.js
+++ next.config.js
@@ -1,7 +1,4 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/tani-portfolio-next-app/' : '',
   images: {
     unoptimized: true,
   },
